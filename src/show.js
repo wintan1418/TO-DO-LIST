f9cb4849584b7aef1821d2a21ab6f57f8a9c7008
oluwadare <wintan1418@gmail.com>
2021-05-18 16:15:38 +0100

    Add logic for header targets

--- src/show.js
+++ src/show.js
@@ -9,3 +9,16 @@ const formatDate = (input) => {
   const result = date.toLocaleDateString('en-US', options);
   return result;
 };
+const displayProjects = (projects) => {
+  const projectsMain = document.querySelector('#all-projects-content');
+  document.querySelector('#all-projects-content').innerHTML = '';
+
+  projects.forEach((project) => {
+    const container = document.createElement('div');
+    container.setAttribute('class', ` ${project.name}-project project-box`);
+    const heading = document.createElement('h3');
+    heading.textContent = project.name;
+
+    const listElement = document.createElement('ul');
+    listElement.id = `${project.name}-list`;
+
