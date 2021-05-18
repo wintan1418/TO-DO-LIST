36769542df36fc4253e28fa70e4cbabc61be8a80
oluwadare <wintan1418@gmail.com>
2021-05-18 16:27:41 +0100

    add local storage function and finish it

--- src/storage.js
+++ src/storage.js
@@ -4,3 +4,19 @@ const saveData = (projects, id) => {
   localStorage.setItem('projects', JSON.stringify(projects));
   localStorage.setItem('currentId', id);
 };
+const initialize = (projects, id) => {
+  if (localStorage.getItem('projects') == null) {
+    projects = [];
+  } else {
+    projects = getData('projects');
+  }
+
+  if (localStorage.getItem('currentId') == null) {
+    id = 0;
+  } else {
+    id = getData('currentId');
+  }
+  return ({ projects, id });
+};
+
+export { initialize, saveData };
