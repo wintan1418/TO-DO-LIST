c7b8e95a8fc8a45cef6417101e9012475e2c89ba
oluwadare <wintan1418@gmail.com>
2021-05-18 16:16:58 +0100

    add logic for targeting divs and dom button

--- src/show.js
+++ src/show.js
@@ -21,4 +21,33 @@ const displayProjects = (projects) => {
 
     const listElement = document.createElement('ul');
     listElement.id = `${project.name}-list`;
+    container.append(heading, listElement);
+    projectsMain.appendChild(container);
+
+    const listItems = project.list;
+    listItems.forEach(item => {
+      const listItem = document.createElement('li');
+      const customDate = formatDate(item.duedate);
+
+      const taskTitle = document.createElement('span');
+      taskTitle.textContent = item.title;
+
+      const taskDate = document.createElement('span');
+      taskDate.textContent = customDate;
+
+      const taskPriority = document.createElement('span');
+      taskPriority.textContent = item.priority;
+
+      listItem.append(taskTitle, taskDate, taskPriority);
+
+      const modifyBtn = document.createElement('button');
+      const deleteBtn = document.createElement('button');
+      modifyBtn.innerHTML = '<img src="https://img.icons8.com/fluent-systems-regular/15/000000/edit-property.png" />';
+      modifyBtn.setAttribute('class', ' btn btn-primary');
+      deleteBtn.innerHTML = '<img src="https://img.icons8.com/material-sharp/15/000000/delete-forever.png" />';
+      deleteBtn.setAttribute('class', 'delete-btn btn btn-danger');
+      listItem.setAttribute('class', 'listItem col-md-12');
+
+      modifyBtn.onclick = () => modifyItem(item, project);
+      deleteBtn.onclick = () => deleteItem(item, project);
 
