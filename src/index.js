3dd6f0b01ec3dbd7c93fe768d23d872d69f281f1
oluwadare <wintan1418@gmail.com>
2021-05-18 16:35:14 +0100

    finish up logic for index.js

--- src/index.js
+++ src/index.js
@@ -125,3 +125,29 @@ const addTaskToProject = (task, project) => {
   saveData(projects, id);
   displayProjects(projects);
 };
+const forminput = () => {
+  const title = document.querySelector('#inputtitle').value.trim();
+  const date = document.querySelector('#inputdate').value;
+  const description = document.querySelector('#inputdescription').value.trim();
+  const note = document.querySelector('#inputnote').value;
+  const priority = document.querySelector('#inputpriority').value;
+  let projectname = document.querySelector('#inputproject').value.trim().toLowerCase();
+
+  const isValidForm = validateForm(title, date, priority);
+  const alert = document.querySelector('#alert');
+
+  if (isValidForm) {
+    projectname = (projectname === '') ? 'default' : projectname;
+
+    const currentTask = todoFactory(title, date, description, note, priority);
+    const list = projectNameList([]);
+    if (!list.includes(projectname)) {
+      const newProject = projectFactory(projectname);
+      projects.push(newProject);
+    }
+    addTaskToProject(currentTask, projectname);
+    setAlert(alert, 'success');
+  } else {
+    setAlert(alert, 'danger');
+  }
+};
