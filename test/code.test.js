c0af44fdaa10c85efdcc676c163bbb8f1ecc6708
oluwadare <wintan1418@gmail.com>
2021-06-09 13:41:08 +0100

    add codes for more test

--- test/code.test.js
+++ test/code.test.js
@@ -1,17 +1,17 @@
 import Project from '../src/projectme';
 
 describe('Project', () => {
-  const project = new Project('Test Project');
+  const project = new Project('My-Test Project');
 
   describe('constructor', () => {
     it('creates a new project instance', () => {
       expect(project instanceof Project).toBe(true);
-      expect(project.name).toEqual('Test Project');
+      expect(project.name).toEqual('My-Test Project');
     });
     it('checks the length of project', () => {
       expect(Project.getAll().lenght).not.toBe(0);
     });
-    test('all returned objects are instances of Project', () => {
+    test('all objects returned back are instances of Project', () => {
       expect(
         Project.getAll().every((project) => project instanceof Project),
       ).toBe(true);
---
+++ test/proj.test.js
@@ -0,0 +1,53 @@
+import Project from '../src/projectme';
+import Todo from '../src/todome';
+
+describe('todo', () => {
+  const todo = new Todo('Test todo');
+
+  describe('constructor', () => {
+    it('creates an instance of a new name', () => {
+      expect(todo instanceof Todo).toBe(true);
+    });
+    it('creates an instance of a descrption', () => {
+      const description = new Todo('do this');
+      expect(description instanceof Todo).toBe(true);
+    });
+    it('creates a new todo instance', () => {
+      const todo = new Todo('do this');
+      expect(todo instanceof Todo).toBe(true);
+    });
+    it('creates an instance of a new priority', () => {
+      const priority = new Todo('do this');
+      expect(priority instanceof Todo).toBe(true);
+    });
+    it('creates an instance of a new date', () => {
+      const date = new Date();
+      expect(date instanceof Date).toBe(true);
+    });
+  });
+
+  describe('getAll', () => {
+    test('gives instance of all returned objects to todo', () => {
+      expect(Todo.getAll().every((todo) => todo instanceof Todo)).toBe(true);
+    });
+  });
+});
+
+describe('delete', () => {
+  const todo = 'I am going south';
+  const todoDelete = Project.getAll().length;
+  it('deletes task from localStorage', () => {
+    todo.delete;
+    const todoCheck = Project.getAll().length;
+    todoDelete !== todoCheck;
+  });
+});
+
+describe('update', () => {
+  let update = 'I love yam';
+  const updateSave = update.id;
+  it('update task from localStorage', () => {
+    update = 'I love yam and egg';
+    updateSave === update;
+  });
+});
