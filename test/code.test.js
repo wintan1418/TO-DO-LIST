4d68160e968e57fd32b7515fa415f6b5943a1467
oluwadare <wintan1418@gmail.com>
2021-06-09 12:59:36 +0100

    add code for test

---
+++ test/code.test.js
@@ -0,0 +1,20 @@
+import Project from '../src/projectme';
+
+describe('Project', () => {
+  const project = new Project('Test Project');
+
+  describe('constructor', () => {
+    it('creates a new project instance', () => {
+      expect(project instanceof Project).toBe(true);
+      expect(project.name).toEqual('Test Project');
+    });
+    it('checks the length of project', () => {
+      expect(Project.getAll().lenght).not.toBe(0);
+    });
+    test('all returned objects are instances of Project', () => {
+      expect(
+        Project.getAll().every((project) => project instanceof Project),
+      ).toBe(true);
+    });
+  });
+});
