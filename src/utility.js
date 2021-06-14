6e2a0bb437b991fc32bfe5109630bfc5210cef08
oluwadare <wintan1418@gmail.com>
2021-06-14 09:54:23 +0100

    re initialize npm

--- src/utility.js
+++
@@ -1,8 +0,0 @@
-export const parser = Range.prototype.createContextualFragment.bind(
-  document.createRange(),
-);
-
-export function randomID() {
-  const digits = String(Math.random()).split('.')[1].substr(0, 10);
-  return `ID${digits}`;
-}
